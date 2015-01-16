861f55a9172a6651fd16a1779c16775677dab3d9
Marcin Szwarc <ms361179@yellow04.mimuw.edu.pl>
2015-01-16 10:06:19 +0100

    Object JS test

--- nasz2.js
+++ nasz2.js
@@ -1,3 +1,110 @@
+function loadJSON(url) 
+{
+	var resp;
+	var xmlHttp;
+	resp  = '';
+	xmlHttp = new XMLHttpRequest();
+
+	if(xmlHttp != null)
+	{
+		xmlHttp.open( "GET", url, false );
+		xmlHttp.send( null );
+		resp = xmlHttp.responseText;
+	}
+	return JSON.parse(resp); 
+}
+function podrozSciezka (from, to, time)
+{
+	var sciezka="http://192.168.1.110/hafas";
+	sciezka+='\?from\='+from;
+	sciezka+='\&to\='+to;
+	sciezka+='\&time\='+time;
+	return sciezka;
+}	
+var WizualizacjaPodrozy = function (from, to, time)
+{
+	this.bazaJSON = loadJSON(podrozSciezka(from, to, time));
+	this.heart=document.getElementById("heart");
+	heart.innerHTML="";
+	this.warstwaVector1 = new ol.source.Vector({});
+	this.warstwaVector2 = new ol.source.Vector({});
+	var podstawa=loadJSON(sciezka);
+	for(var i=0; i < podstawa.length; i++)
+	{
+		var nowalinia=document.createElement("DIV");
+		var nowaliniaid=document.createElement("DIV");
+		nowaliniaid.innerHTML=podstawa[i].line;
+		nowaliniaid.className="hafasliniaid";
+		nowalinia.appendChild(nowaliniaid);
+		nowalinia.className="hafaslinia";
+		nowalinia.setAttribute("paletteid", getpaletteid(i, podstawa.length));
+		heart.appendChild(nowalinia);
+		var tablica1 = [];
+		var tablica2 = [];
+		for(var j=0; j < podstawa[i].route.length; j++)
+		{
+			var tmp=ol.proj.transform([podstawa[i].route[j].lon, podstawa[i].route[j].lat], 'EPSG:4326', 'EPSG:3857');
+			tablica1[tablica1.length] = tmp;
+			if(podstawa[i].route[j].type=="stop")
+			{
+				tablica2[tablica2.length] = tmp;
+				var nowystop=document.createElement("DIV");
+				var nowystopname=document.createElement("DIV");
+				var nowystoptime=document.createElement("DIV");
+				nowystopname.innerHTML=podstawa[i].route[j].name;
+				var tim=podstawa[i].route[j].time;
+				tim/=60;
+				var mins=tim%60;
+				var hours=Math.floor(tim/60);
+				nowystoptime.innerHTML=hours+":"+mins;
+				nowystop.appendChild(nowystopname);
+				nowystop.appendChild(nowystoptime);
+				nowystop.className="hafasstop";
+				nowalinia.appendChild(nowystop);
+			}
+		}
+		var fea_tmp1 = new ol.Feature({geometry: new ol.geom.LineString(tablica1)});
+		fea_tmp1.setStyle(new ol.style.Style({
+		stroke: new ol.style.Stroke({
+		color: getpalette(i, podstawa.length),
+		width: 4
+		}), text: new ol.style.Text({text: podstawa[i].line,  font: '18px helvetica,sans-serif,bold', fill: new ol.style.Fill({
+			                            color: getpalette(i, podstawa.length)
+						                            }),
+									                            stroke: new ol.style.Stroke({
+													                                color: '#fff',
+																	                            width: 6
+																				                            })})
+	}));
+		var fea_tmp2 = new ol.Feature({name: 'My Polygon', geometry: new ol.geom.LineString(tablica1)});
+		fea_tmp2.setStyle(new ol.style.Style({
+		stroke: new ol.style.Stroke({
+		color: '#fff',
+		width: 7
+		})}));
+		var fea_tmp3 = new ol.Feature({geometry: new ol.geom.MultiPoint(tablica2)});
+		fea_tmp3.setStyle(
+	new ol.style.Style({
+    image: new ol.style.Circle({
+      fill: new ol.style.Fill({
+        color: getpalette(i, podstawa.length)
+      }),
+      radius: 8,
+      stroke: new ol.style.Stroke({
+        color: '#fff',
+        width: 2
+      })
+    })
+  }));
+		bv1.addFeature(fea_tmp1);
+		bv1.addFeature(fea_tmp3);
+		bv2.addFeature(fea_tmp2);
+	}
+	return [bv1, bv2];
+}
+
+}
+
 var palette = ['#ab0d15'
 ,'#F7931D'
 ,'#FFF200'
