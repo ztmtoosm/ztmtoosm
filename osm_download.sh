4eea3ae15e297e0352ff9312581d83ec56e46387
root <root@vps134914.ovh.net>
2015-06-14 16:22:15 +0200

    autoup2

--- osm_download.sh
+++ osm_download.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 readarray -t a < $1/data/lista9.ooo
 pol="wget http://overpass-api.de/api/map?bbox=20.5225,51.9,21.5250,52.4953 -O "$1"/data/latest0.osm"
+pol2="wget http://overpass-api.de/api/map?bbox=18.3691,54.2175,18.9439,54.4968 -O "$1"/data/gdansk.osm"
 $pol
+$pol2
 for i in "${a[@]}"
 do
 	wget http://api.openstreetmap.org/api/0.6/relation/"${i}" -O $1"/data/"test"${i}".osm
