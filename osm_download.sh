e6047235b52743710efd655f7b00c45118a54884
Marcin Szwarc <marcinek.szwarc@gmail.com>
2015-06-23 22:34:55 +0200

    poprawki skryptów

--- osm_download.sh
+++ osm_download.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
-readarray -t a < $1/data/lista9.ooo
-pol="wget http://overpass-api.de/api/map?bbox=20.5225,51.9,21.5250,52.4953 -O "$1"/data/latest0.osm"
-pol2="wget http://overpass-api.de/api/map?bbox=18.3691,54.2175,18.9439,54.4968 -O "$1"/data/gdansk.osm"
-pol3="wget http://overpass-api.de/api/map?bbox=14.2946,53.20,14.81,53.64 -O "$1"/data/szczecin.osm"
-$pol
-$pol2
-$pol3
-for i in "${a[@]}"
-do
-	wget http://api.openstreetmap.org/api/0.6/relation/"${i}" -O $1"/data/"test"${i}".osm
-done
-b='../osmconvert '$1'/data/latest0.osm'
-for i in "${a[@]}"
-do
-	b+=" "$1"/data/test"${i}".osm"
-done
-b+=' -o='$1'/data/latest.osm'
-$b
-for i in "${a[@]}"
-do
-	rm $1"/data/test"${i}".osm"
-done
-
-c=$1"/data/ztm_download1.txt"
-$1"/bin/"RouteInfoGenerator $c $1"/data/"latest.osm $1"/data/wyj4.osm" $1"/www" "-all"
+$1"/download_map_osm.sh" $1 warszawa 20.5225 51.9 21.5250 52.4953 3651332 3651328 3651327 3651329 3651326 3651335 3651336 3651333 4656333
+$1"/download_map_osm.sh" $1 szczecin 14.2946 53.20 14.81 53.64 5291742
+$1"/bin/"RouteInfoGenerator $"/data/"warszawa.txt $1"/data/"warszawa.osm $1"/data/wyj4.osm" $1"/www" "-all"
 $1"/bin/"Generator2 $1"/data/"szczecin $1"/data/"szczecin.osm $1"/www" Szczecin ref
-$1"/bin/"Generator2 $1"/data/"gdansk $1"/data/"gdansk.osm $1"/www" Gdańsk ref:ztm
-# $1"/bin/"Generator2 $1"/data/"ztm_download1.txt $1"/data/"warszawa.osm $1"/www" Warszawa ref
+# $1"/bin/"Generator2 $1"/data/"gdansk $1"/data/"gdansk.osm $1"/www" Gdańsk ref:ztm
+$1"/bin/"Generator2 $1"/data/"ztm_download1.txt $1"/data/"warszawa.osm $1"/www" Warszawa ref
