1a5b9d65ea4f40f48a63d809aa7941164f3307a3
root <root@vps134914.ovh.net>
2015-06-24 11:31:43 +0200

    poprawki skryptów dalej

--- download_osm_map.sh
+++ download_osm_map.sh
@@ -1,19 +1,18 @@
 pol="wget http://overpass-api.de/api/map?bbox="$3","$4","$5","$6" -O "$1"/data/test.osm"
 $pol
-for ((i=7;i<$#;i++))
+for ((i=7;i<=$#;i++))
 do
-	wget http://api.openstreetmap.org/api/0.6/relation/"${i}" -O $1"/data/"test"${!i}".osm
+	wget http://api.openstreetmap.org/api/0.6/relation/"${!i}" -O $1"/data/"test"${!i}".osm
 done
 b='osmconvert '$1'/data/test.osm'
-for ((i=7;i<$#;i++))
+for ((i=7;i<=$#;i++))
 do
 	b+=" "$1"/data/test"${!i}".osm"
 done
+b+=' -o='$1'/data/'$2'.osm'
 $b
-for ((i=7;i<$#;i++))
+for ((i=7;i<=$#;i++))
 do
 	rm $1"/data/test"${!i}".osm"
 done
-
-b+=' -o='$1'/data/'$2'.osm'
 rm $1'/data/test.osm'
--- osm_download.sh
+++ osm_download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-$1"/download_map_osm.sh" $1 warszawa 20.5225 51.9 21.5250 52.4953 3651332 3651328 3651327 3651329 3651326 3651335 3651336 3651333 4656333
-$1"/download_map_osm.sh" $1 szczecin 14.2946 53.20 14.81 53.64 5291742
-$1"/bin/"RouteInfoGenerator $"/data/"warszawa.txt $1"/data/"warszawa.osm $1"/data/wyj4.osm" $1"/www" "-all"
+$1"/download_osm_map.sh" $1 warszawa 20.5225 51.9 21.5250 52.4953 3651332 3651328 3651327 3651329 3651326 3651335 3651336 3651333 4656333 3651331 
+$1"/download_osm_map.sh" $1 szczecin 14.2946 53.20 14.81 53.64 5291742
+$1"/bin/"RouteInfoGenerator $1"/data/"warszawa.txt $1"/data/"warszawa.osm $1"/data/wyj4.osm" $1"/www" "-all"
 $1"/bin/"Generator2 $1"/data/"szczecin $1"/data/"szczecin.osm $1"/www" Szczecin ref
-# $1"/bin/"Generator2 $1"/data/"gdansk $1"/data/"gdansk.osm $1"/www" Gdańsk ref:ztm
-$1"/bin/"Generator2 $1"/data/"ztm_download1.txt $1"/data/"warszawa.osm $1"/www" Warszawa ref
+#$1"/bin/"Generator2 $1"/data/"gdansk $1"/data/"gdansk.osm $1"/www" Gdańsk ref:ztm
+$1"/bin/"Generator2 $1"/data/"warszawa.txt $1"/data/"warszawa.osm $1"/www" Warszawa ref
