46b3d33fef3d961903c6b3b7e6a93a02796d9981
root <root@vps134914.ovh.net>
2015-06-26 12:07:59 +0200

    foo

--- osm_download.sh
+++ osm_download.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-$1"/download_osm_map.sh" $1 warszawa 20.5225 51.9 21.5250 52.4953 3651332 3651328 3651327 3651329 3651326 3651335 3651336 3651333 4656333 3651331 
-$1"/download_osm_map.sh" $1 szczecin 14.2946 53.20 14.81 53.64 5291742
-$1"/bin/"RouteInfoGenerator $1"/data/"warszawa.txt $1"/data/"warszawa.osm Warszawa $1"/www" "-all"
-$1"/bin/"RouteInfoGenerator $1"/data/"szcecin $1"/data/"szczecin.osm Szczecin $1"/www" "-all"
-$1"/bin/"Generator2 $1"/data/"szczecin $1"/data/"szczecin.osm $1"/www" Szczecin ref
+#$1"/download_osm_map.sh" $1 warszawa 20.5225 51.9 21.5250 52.4953 3651332 3651328 3651327 3651329 3651326 3651335 3651336 3651333 4656333 3651331 
+#$1"/download_osm_map.sh" $1 szczecin 14.2946 53.20 14.81 53.64 5291742
+#$1"/bin/"RouteInfoGenerator $1"/data/"warszawa.txt $1"/data/"warszawa.osm Warszawa $1"/www" "-all"
+$1"/bin/"RouteInfoGenerator $1"/data/"szczecin $1"/data/"szczecin.osm Szczecin $1"/www" "-all"
+#$1"/bin/"Generator2 $1"/data/"szczecin $1"/data/"szczecin.osm $1"/www" Szczecin ref
 #$1"/bin/"Generator2 $1"/data/"gdansk $1"/data/"gdansk.osm $1"/www" Gdańsk ref:ztm
-$1"/bin/"Generator2 $1"/data/"warszawa.txt $1"/data/"warszawa.osm $1"/www" Warszawa ref
+#$1"/bin/"Generator2 $1"/data/"warszawa.txt $1"/data/"warszawa.osm $1"/www" Warszawa ref
