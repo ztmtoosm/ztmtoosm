7207c759effd6cb54276f706d2ec2b49822344c5
Marcin Szwarc <marcinek.szwarc@gmail.com>
2015-08-18 20:43:46 +0200

    Poprawki parsera html

--- src/RouteInfoGenerator/HTMLHeadersRouteInfo.cpp
+++ src/RouteInfoGenerator/HTMLHeadersRouteInfo.cpp
@@ -75,5 +75,9 @@ string HtmlExtraGenerator::loadTemplate(string path)
 }
 bool okVariable(string str)
 {
-	
+	for(int i=0; i<str.length(); i++) {
+		if(!isdigit(str[i]))
+			return false;
+	}
+	return true;
 }
