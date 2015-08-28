c6cf5db77164b05f427f7c815c9f98261911db8b
Marcin Szwarc <marcinek.szwarc@gmail.com>
2015-08-28 17:39:26 +0200

    foo

--- src/RouteInfoGenerator/RouteInfoGenerator.cpp
+++ src/RouteInfoGenerator/RouteInfoGenerator.cpp
@@ -840,7 +840,7 @@ struct galk
 		{
 			if(licznik>0)
 				message+=" ,";
-			message+=htmlgen::link(miasto+".html#"+it1, osmStopData[it1].name+" ("+it1+")", "");
+			message+=htmlgen::link(miasto+".html#"+it1, bazaZtm->przystanki[it1].name+" ("+it1+")", "");
 			licznik++;
 		}
 		gen.loadedVariables[0]=linia+"niewygenerowane";
@@ -1140,6 +1140,7 @@ struct galk
 			jsonStream<<"\""<<it1.str<<"\"";
 			generujLinie(it1.str);
 			string message1 = dodajInfoNormalne(przegl0.relacjeDlaLinii[it1.str], it1.str, htmlGenerator, przegl0.badRelations);
+			message1 += dodajInfoRoznice(przegl0.onlyOsmStop[it1.str], przegl0.onlyZtmStop[it1.str], it1.str, htmlGenerator);
 			dodajLinieDoHTML(lineHTMLStream, 2, it1.str, message1, htmlGenerator);
 			licznikx++;
 		}
