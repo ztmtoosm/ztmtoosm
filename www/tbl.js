b67ccde23556f213bde71cb6069e56c4ca2e131f
root <root@vps134914.ovh.net>
2015-09-19 12:47:19 +0200

    na ser

--- www/tbl.js
+++ www/tbl.js
@@ -9,6 +9,18 @@ var newGroupItem = function(ojciec, data)
 	ojciec.appendChild(li);
 }
 
+var osmUrl = function(type, id)
+{
+	var type2 = "node";
+	if(type=="W")
+		type2="way";
+	if(type=="R")
+		type2="relation";
+	if(id==0 || id=="0" || id==undefined || type==undefined)
+		return "BRAK";
+	return "<a href=\"http://openstreetmap.org/"+type2+"/"+id+"\">"+id+"</a>";
+}
+
 var tbl = function(url)
 {
 	jQuery.get(url.url, function(data) {
@@ -24,8 +36,9 @@ var tbl = function(url)
 		divHead.innerHTML = data[i].id+" "+data[i].name;
 		div.appendChild(divHead);
 		div.appendChild(divGroup);
-		newGroupItem(divGroup, data[i].name);
-		newGroupItem(divGroup, data[i].stop_position);
+		newGroupItem(divGroup, "bus_stop name: "+data[i].bus_stop_name+", stop_position name: "+data[i].stop_position_name+", platform name: "+data[i].platform_name);
+		newGroupItem(divGroup, "bus_stop "+osmUrl('N', data[i].bus_stop)+" stop_position "+osmUrl('N', data[i].stop_position)+" platform "+osmUrl(data[i].platform_type, data[i].platform));
+		newGroupItem(divGroup, "BS_SP="+data[i].BS_SP+" SP_PL="+data[i].SP_PL+" PL_BS="+data[i].PL_BS);
 		for(var j=0; j<data[i].kierunki.length; j++)
 			newGroupItem(divGroup, data[i].kierunki[j]);
 		newGroupItem(divGroup, data[i].lon);
