8e9e8e1efb2992c6dad2793ed565ae11427c366f
Marcin Szwarc <marcinek.szwarc@gmail.com>
2016-03-08 17:55:07 +0100

    next sqlite

--- src/ntt/ToLite.cpp
+++ src/ntt/ToLite.cpp
@@ -13,12 +13,26 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
    return 0;
 }
 
+void sqlite_execute_easy(sqlite3* db, string pol, int errorcode)
+{
+  char *zErrMsg = 0;
+  int rc;
+  rc = sqlite3_exec(db, pol.c_str(), callback, 0, &zErrMsg);
+  if(rc != SQLITE_OK)
+  {
+    //cerr << "SQL error: " << zErrMsg << endl;
+    sqlite3_free(zErrMsg);
+    exit(errorcode);
+  }
+}
+
 class MyHand : public ScheduleHandler
 {
   sqlite3* db;
   public:
   MyHand(sqlite3* db2) : db(db2) {}
   void nowa_linia(string nazwa, vector <vector <string> > trasy);
+  void nowy_przystanek(przystanek nowy);
 };
 
 void MyHand::nowa_linia(string nazwa, vector <vector <string> > trasy)
@@ -27,20 +41,20 @@ void MyHand::nowa_linia(string nazwa, vector <vector <string> > trasy)
   {
     for(int j=0; j<trasy[i].size(); j++)
     {
-      char *zErrMsg = 0;
-      int rc;
       stringstream sql;
       sql << "INSERT INTO OPERATOR_ROUTES(ROUTE_ID, DIRECTION, STOP_ON_DIRECTION_NUMBER, STOP_ID) VALUES('";
       sql << nazwa << "', " << i+1 << ", " << j+1 << ", '" << trasy[i][j] << "');";
-      rc = sqlite3_exec(db, sql.str().c_str(), callback, 0, &zErrMsg);
-      if(rc != SQLITE_OK)
-      {
-        //cerr << "SQL error: " << zErrMsg << endl;
-        sqlite3_free(zErrMsg);
-        exit(3);
-      }
+      sqlite_execute_easy(db, sql.str(), 3);
     }
   }
+}
+
+void MyHand::nowy_przystanek(przystanek nowy)
+{
+  stringstream sql;
+  sql << "INSERT INTO OPERATOR_STOPS(STOP_ID, NAME, LON, LAT, COORDINATES_QUALITY, MORE_INFO) VALUES('";
+  sql << nowy.id << "', '" << nowy.name << "', " << nowy.lon << ", " << nowy.lat << ", " << nowy.wsp_jakosc << ", '" << nowy.stopinfo << "');";
+  sqlite_execute_easy(db, sql.str(), 4);
 }
 
 int main(int argc, char** argv)
@@ -64,30 +78,33 @@ int main(int argc, char** argv)
                "DIRECTION INTEGER," \
                "STOP_ON_DIRECTION_NUMBER INTEGER," \
                "STOP_ID CHAR(10));";
-  rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
-  if(rc != SQLITE_OK)
-  {
-    //cerr << "SQL error: " << zErrMsg << endl;
-    sqlite3_free(zErrMsg);
-    return 2;
-  }
+      sqlite_execute_easy(db, sql, 2);
+
+      sql = "CREATE TABLE IF NOT EXISTS OPERATOR_STOPS("  \
+                     "STOP_ID CHAR(10) PRIMARY KEY," \
+                     "NAME CHAR(100)," \
+                     "LON REAL," \
+                     "LAT REAL," \
+                     "COORDINATES_QUALITY INTEGER," \
+                     "MORE_INFO TEXT;";
+            sqlite_execute_easy(db, sql, 2);
 
   MyHand hnd(db);
-if(tmp0 == "Warszawa")
-{
-  ScheduleReaderWarszawa reader(tmp1, &hnd);
-  reader.run();
-}
-if(tmp0 == "Gdańsk")
-{
-  ScheduleReaderGdansk reader(tmp1, &hnd);
-  reader.run();
-}
-if(tmp0 == "Szczecin")
-{
-  ScheduleReaderSzczecin reader(tmp1, &hnd);
-  reader.run();
-}
+  if(tmp0 == "Warszawa")
+  {
+    ScheduleReaderWarszawa reader(tmp1, &hnd);
+    reader.run();
+  }
+  if(tmp0 == "Gdańsk")
+  {
+    ScheduleReaderGdansk reader(tmp1, &hnd);
+    reader.run();
+  }
+  if(tmp0 == "Szczecin")
+  {
+    ScheduleReaderSzczecin reader(tmp1, &hnd);
+    reader.run();
+  }
   sqlite3_close(db);
   return 0;
 }
