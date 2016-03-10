3e1281b6804ad9486dfdefc21e9db556c7eafbc7
Marcin Szwarc <ms361179@khaki08.mimuw.edu.pl>
2016-03-10 14:18:59 +0100

    t

--- src/ntt/OsmLite.cpp
+++ src/ntt/OsmLite.cpp
@@ -7,6 +7,9 @@ int main(int argc, char** argv)
   string sciezka = argv[1];
 
   osm_base osmData(sciezka);
+  map<string, OsmStopData> data = loadOsmStopData(&osmData);
+  
+
 
   string tmp2 = argv[2];
   sqlite3 *db;
@@ -26,5 +29,19 @@ int main(int argc, char** argv)
                "STOP_POSITION_NAME CHAR(255)," \
                ");";
       sqlite_execute_easy(db, sql, 2);
+      
+        for(auto& it1 : data)
+  {
+	  string aktName = it1.first;
+	  OsmStopData aktData = it1.second;
+	  stringstream sql;
+      sql << "INSERT INTO OSM_STOPS(NORMAL_STOP, STOP_POSITION, REF_ID, STOP_POSITION_NAME) VALUES('";
+      sql << aktData.bus_stop << "', " << aktData.stop_position << ", " << aktName << ", %q);";
+      char *zSQL = sqlite3_mprintf(sql.str().c_str(), aktData.name.c_str());
+	  string zSQL2 = zSQL;
+	  sqlite3_free(zSQL);
+      sqlite_execute_easy(db, zSQL2, 3);
+  }
+  
   sqlite3_close(db);
 }
