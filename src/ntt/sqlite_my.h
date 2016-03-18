894994443abb3149fd6176a85581183d7ade252d
Marcin Szwarc <marcinek.szwarc@gmail.com>
2016-03-18 18:25:58 +0100

    si

--- src/ntt/sqlite_my.h
+++ src/ntt/sqlite_my.h
@@ -4,5 +4,5 @@
 static int callback(void *NotUsed, int argc, char **argv, char **azColName);
 void sqlite_execute_easy(sqlite3* db, string pol, int errorcode);
 void create_tables_if_not_exist(sqlite3* db);
-string if0(int val);
+string if0(long long val);
 #endif
